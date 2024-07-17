43f626e1dac98cb6487c82031376839e6058b00c
YFayzulla <yoldashevFayzulla0@gmail.com>
2024-07-17 19:18:08 +0500

    Service mvc done

---
+++ app/Http/Controllers/ServiceController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Service;
+use Illuminate\Http\Request;
+
+class ServiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $services = Service::all();
+        return view('services.index', compact('services'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('services.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            "cost" => 'required',
+        ]);
+
+        Service::create($request->all());
+
+        return redirect()->route('services.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Service $service)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Service $service)
+    {
+        return view('services.edit', compact('service'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Service $service)
+    {
+        $request->validate([
+            'name' => 'required',
+            "cost" => 'required',
+        ]);
+
+        $service->update($request->all());
+
+        return redirect()->route('services.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Service $service)
+    {
+        $service->delete();
+        return redirect()->route('services.index');
+    }
+}
