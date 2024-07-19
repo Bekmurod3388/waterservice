c1ef58d6a1e5ca8aa5648d0e0daf6ec177c287fd
YFayzulla <yoldashevFayzulla0@gmail.com>
2024-07-19 15:27:40 +0500

    web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\FilterController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ServiceController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [LoginController::class,'loginPage'])->name('loginPage');
@@ -13,6 +15,8 @@ Route::group(['middleware' => 'auth'], function () {
     });
 
     Route::resource('/client', ClientController::class);
+    Route::resource('/service', ServiceController::class);
+    Route::resource('/filter', FilterController::class);
 
     Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 });
