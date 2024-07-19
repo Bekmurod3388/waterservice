be47a4838e020461ba91612e06cf54298cefedb1
Faxriddin077 <59280046+Faxriddin077@users.noreply.github.com>
2024-07-19 19:37:32 +0500

    mock data seeder

---
+++ database/seeders/MockDataSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Client;
+use App\Models\Filter;
+use App\Models\Point;
+use App\Models\Region;
+use App\Models\Service;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MockDataSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $filter = Filter::query()->firstOrCreate([
+            'name' => 'test filter',
+            'cost' => 123000
+        ]);
+
+        Service::query()->firstOrCreate([
+            'name' => 'test servis',
+            'cost' => 654000
+        ]);
+
+        $client = Client::query()->firstOrCreate([
+            'name' => 'test client',
+            'phone' => '991324657'
+        ]);
+
+        Point::query()->firstOrCreate([
+            'client_id' => $client->id,
+            'region_id' => Region::query()->inRandomOrder()->first(),
+            'address' => 'test address',
+            'filter_id' => $filter->id,
+            'filter_expire' => 3,
+            'filter_expire_date' => now()
+        ]);
+    }
+}
