cd3624032e43b846d14c418ed08e9a6b79da101e
Azamat <azamatrahimov80@gmail.com>
2024-07-20 22:08:50 +0500

    User added change password

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -58,3 +58,28 @@
     </div>
 
 @endsection
+
+@section('script')
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            const passwordToggleIcons = document.querySelectorAll('.form-password-toggle .input-group-text');
+
+            passwordToggleIcons.forEach(function (icon) {
+                icon.addEventListener('click', function () {
+                    const input = icon.parentElement.querySelector('input');
+                    const iconElement = icon.querySelector('i');
+
+                    if (input.type === 'password') {
+                        input.type = 'text';
+                        iconElement.classList.remove('bx-hide');
+                        iconElement.classList.add('bx-show');
+                    } else {
+                        input.type = 'password';
+                        iconElement.classList.remove('bx-show');
+                        iconElement.classList.add('bx-hide');
+                    }
+                });
+            });
+        });
+    </script>
+@endsection
