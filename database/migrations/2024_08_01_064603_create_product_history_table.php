97cbc5573693af6c09ba78251344c650d9c06582
YFayzulla <yoldashevFayzulla0@gmail.com>
2024-08-01 11:49:35 +0500

    product_history  database added

---
+++ database/migrations/2024_08_01_064603_create_product_history_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_history', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('user_id');
+            $table->Biginteger('cost_price');
+            $table->Biginteger('purchase_price');
+            $table->integer('differance');
+            $table->integer('before');
+            $table->integer('after');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_history');
+    }
+};
