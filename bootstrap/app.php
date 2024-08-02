430534e1b479eeb5f0aafdecd69d080f262f2f27
Faxriddin077 <59280046+Faxriddin077@users.noreply.github.com>
2024-08-02 18:23:36 +0500

    logs

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(LoggerMiddleware::class);
+//        $middleware->append(LoggerMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ServiceController;
 use App\Http\Middleware\CheckTokenMiddleware;
+use App\Http\Middleware\LoggerMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('login', [LoginController::class, 'loginPage'])->name('loginPage');
@@ -47,7 +48,7 @@ Route::group(['middleware' => 'auth'], function () {
     Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 });
 
-Route::prefix('mobile/{token}')->middleware(CheckTokenMiddleware::class)->group(function () {
+Route::prefix('mobile/{token}')->middleware([CheckTokenMiddleware::class, LoggerMiddleware::class])->group(function () {
 
     Route::prefix('agent')->group(function () {
         Route::get('index', [MobileAgentController::class, 'index'])->name('mobile.agent.index');
