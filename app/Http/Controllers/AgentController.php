5b1ce7234ef117679a648692e2bc2fcf798d6f15
Faxriddin077 <59280046+Faxriddin077@users.noreply.github.com>
2024-08-02 18:05:51 +0500

    agents

--- app/Http/Controllers/AgentController.php
+++ app/Http/Controllers/AgentController.php
@@ -13,15 +13,14 @@ class AgentController extends Controller
             ->whereHas('roles', function ($query) {
                 $query->where('name', 'agent');
             })
-            ->with(['tasks' => function ($query) {
-                $query->select(
-                    'user_id',
-                    DB::raw('COUNT(*) as total_tasks'),
-                    DB::raw('SUM(CASE WHEN is_completed = 0 THEN 1 ELSE 0 END) as incomplete_tasks'),
-                    DB::raw('SUM(CASE WHEN is_completed = 1 THEN 1 ELSE 0 END) as complete_tasks')
-                )
-                    ->groupBy('user_id');
-            }])
+            ->withCount([
+                'tasks as incomplete_tasks' => function ($query) {
+                    $query->where('is_completed', 0);//->whereDate('created_at', today()->format('Y-m-d'));
+                },
+                'tasks as complete_tasks' => function ($query) {
+                    $query->where('is_completed', 1);//->whereDate('created_at', today()->format('Y-m-d'));
+                }
+            ])
             ->paginate(10);
 
         return view('agents.index', [
