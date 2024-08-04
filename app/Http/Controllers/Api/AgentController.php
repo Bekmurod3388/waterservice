8abce9e4b4f0a0a91064f27cd440f2823e8d9a3b
Jamshidbek <jamshidbekollanazarov@gmail.com>
2024-08-04 18:16:04 +0500

    api task updated

--- app/Http/Controllers/Api/AgentController.php
+++ app/Http/Controllers/Api/AgentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Service;
 use App\Models\Task;
 
 class AgentController extends Controller
@@ -13,7 +14,15 @@ class AgentController extends Controller
             'tasks' => Task::with('point', 'client')->where('agent_id', auth()->id())->get()
         ]);
     }
+
     public function task(Task $task){
-        return response()->json($task);
+        return response()->json(
+            [
+                "task" => $task,
+                "client" => $task->client(),
+                "point" => $task->point(),
+                "services" => $task->services(),
+                "all_services" => Service::all()
+            ]);
     }
 }
