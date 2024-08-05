b54140a741a12f61932ecc1401ebded5e83d3813
Faxriddin077 <59280046+Faxriddin077@users.noreply.github.com>
2024-08-05 13:42:09 +0500

    added session error and fix bug transaction fail

--- app/Http/Controllers/AgentController.php
+++ app/Http/Controllers/AgentController.php
@@ -17,6 +17,7 @@ class AgentController extends Controller
     )
     {
     }
+
     public function index()
     {
         $agents = User::role('agent')
@@ -35,13 +36,21 @@ class AgentController extends Controller
         ]);
 
     }
-    public function products(User $agent){
-        $agent_products = AgentProduct::query()->where('agent_id',$agent->id)->paginate(10);
+
+    public function products(User $agent)
+    {
+        $agent_products = AgentProduct::query()->where('agent_id', $agent->id)->paginate(10);
         $products = Product::all();
-        return view('agents.products.index',['agent_products'=>$agent_products,'products'=>$products,'agent'=>$agent]);
+        return view('agents.products.index', [
+            'agent_products' => $agent_products,
+            'products' => $products,
+            'agent' => $agent
+        ]);
     }
-    public function product_store(StoreRequest $request, User $agent){
-        $res = $this->service->create($request->validated(),$agent);
-        return redirect()->route('agent.products', [$agent->id])->with($res);
+
+    public function product_store(StoreRequest $request, User $agent)
+    {
+        $res = $this->service->create($request->validated(), $agent);
+        return redirect()->back()->with($res['key'], $res['message']);
     }
 }
--- resources/views/alerts/success-alert.blade.php
+++ resources/views/alerts/success-alert.blade.php
@@ -4,3 +4,10 @@
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Yopmoq"></button>
     </div>
 @endif
+
+@if(session('error'))
+    <div id="success-alert" class="alert alert-danger alert-dismissible" role="alert">
+        <strong>Xatolik!</strong> {{ session('error') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Yopmoq"></button>
+    </div>
+@endif
