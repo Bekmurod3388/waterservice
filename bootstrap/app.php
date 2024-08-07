253f064e67637f14133d18b5e19f63dff3e8e86e
Faxriddin077 <59280046+Faxriddin077@users.noreply.github.com>
2024-08-07 21:15:23 +0500

    logger and logs

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-//        $middleware->append(LoggerMiddleware::class);
+        $middleware->api(prepend: [
+            LoggerMiddleware::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
