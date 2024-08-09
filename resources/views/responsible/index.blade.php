dd9c194889b7d301f161314aa3622a9f259805e1
S.Farruhbek <satimbayevfarruhbek@gmail.com>
2024-08-09 22:23:24 +0500

    Name of the months was changed to Uzbek

--- resources/views/responsible/index.blade.php
+++ resources/views/responsible/index.blade.php
@@ -39,12 +39,28 @@
                     </tr>
                     </thead>
                     <tbody class="table-border-bottom-0">
+                    <?php
+                        $months = [
+                            '01' => 'Yanvar',
+                            '02' => 'Fevral',
+                            '03' => 'Mart',
+                            '04' => 'Aprel',
+                            '05' => 'May',
+                            '06' => 'Iyun',
+                            '07' => 'Iyul',
+                            '08' => 'Avgust',
+                            '09' => 'Sentyabr',
+                            '10' => 'Oktyabr',
+                            '11' => 'Noyabr',
+                            '12' => 'Dekabr',
+                        ];
+                    ?>
                     @forelse($responsible as $val)
                         <tr>
                             <td>{{$val->id}}</td>
                             <td>{{$val->operator->name}}</td>
                             <td>{{$val->cashier->name}}</td>
-                            <td>{{\Carbon\Carbon::createFromFormat('Y-m', $val->month)->format('F')}}</td>
+                            <td>{{ $months[\Carbon\Carbon::createFromFormat('Y-m', $val->month)->format('m')] }}</td>
                             <td>
                                 <button
                                     onclick="editModal({{$val->id}})"
